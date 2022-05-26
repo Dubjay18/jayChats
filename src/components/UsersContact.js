8c1dc5888803f75eb9aba00aec5a23092c902ea3
Dubjay18 <jejeniyi7@gmail.com>
2022-05-26 13:25:15 +0100

    sidebar

--- src/components/UsersContact.js
+++ src/components/UsersContact.js
@@ -14,52 +14,55 @@ function UsersContact({ handleChatChange, contacts, currentUser }) {
   };
   return (
     <>
-      {userImage && currentUser && (
-        <div>
-          <h1 className="md:text-3xl font-poppins text-emerald-300">
-            JayChats
-          </h1>
-
-          <div className="text-emerald-400 font-poppins h-[670px] overflow-scroll remove-scroll">
-            {contacts.map((contact, i) => {
-              return (
-                <div>
-                  <div
-                    className="flex items-center p-4 bg-zinc-700 my-2 hover:bg-zinc-500 rounded-md active:bg-zinc-200 cursor-pointer"
-                    onClick={() => changeCurrentChat(i, contact)}
-                  >
+      <div>
+        <h1 className="md:text-3xl font-poppins text-emerald-300">JayChats</h1>
+        {userImage && currentUser ? (
+          <>
+            {" "}
+            <div className="text-emerald-400 font-poppins h-[670px] overflow-scroll remove-scroll">
+              <h1 className="md:text-2xl">Users</h1>
+              {contacts.map((contact, i) => {
+                return (
+                  <div>
                     <div
-                      className={`contact w-10 mx-6  ${
-                        i === currentSelected ? "selected" : ""
-                      }`}
-                      key={i}
+                      className="flex items-center p-4 bg-zinc-700 my-2 hover:bg-zinc-500 rounded-md active:bg-zinc-200 cursor-pointer"
+                      onClick={() => changeCurrentChat(i, contact)}
                     >
-                      <img
-                        src={`data:image/svg+xml;base64,${contact?.avatarImage}`}
-                        alt="avatar"
-                      />
-                    </div>
-                    <div>
-                      <h3>{contact.username}</h3>
-                    </div>
-                  </div>{" "}
-                </div>
-              );
-            })}
-          </div>
-          <div className="flex items-center text-emerald-400 font-poppins mt-2 p-4">
-            <div className="w-10 mx-6">
-              <img
-                src={`data:image/svg+xml;base64,${userImage}`}
-                alt="avatar"
-              />
+                      <div
+                        className={`contact w-10 mx-6  ${
+                          i === currentSelected ? "selected" : ""
+                        }`}
+                        key={i}
+                      >
+                        <img
+                          src={`data:image/svg+xml;base64,${contact?.avatarImage}`}
+                          alt="avatar"
+                        />
+                      </div>
+                      <div>
+                        <h3>{contact.username}</h3>
+                      </div>
+                    </div>{" "}
+                  </div>
+                );
+              })}
             </div>
-            <div>
-              <h1>{currentUser}</h1>
+            <div className="flex items-center text-emerald-400 font-poppins mt-2 p-4">
+              <div className="w-10 mx-6">
+                <img
+                  src={`data:image/svg+xml;base64,${userImage}`}
+                  alt="avatar"
+                />
+              </div>
+              <div>
+                <h1>{currentUser}</h1>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </>
+        ) : (
+          <div>no users</div>
+        )}
+      </div>
     </>
   );
 }
